64665ad3a5913cc2a3dbfada5c00374f707f2173
Santiago Rios Valero <riossanval@gmail.com>
2025-02-22 15:12:52 -0500

    UPDATE: Enhance translateTask function to translate task name and description; update Firestore with both translations

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -11,24 +11,34 @@ const translate = new Translate();
 // Detecta cuando se crea un documento en la colección tasks
 export const translateTask = functions.firestore
   .document("tasks/{taskId}")
-  .onCreate(async (snap, context) => {
+  .onCreate(async (snap) => {
     const task = snap.data();
-    if (!task || !task.description) {
+    if (!task) {
       console.warn("No hay texto para traducir.");
       return null;
     }
 
     try {
       // Traduce el texto de español al inglés
-      const [translation] = await translate.translate(task.description, "en");
+      const [translationName] = await translate.translate(task.name, "en");
+      const [translationDesc] = await translate.translate(
+        task.description,
+        "en",
+      );
 
       // Guardar la traducción en Firestore en le campo translatedDescription
-      await snap.ref.update({translatedDescription: translation});
+      await snap.ref.update({
+        translatedDescription: translationDesc,
+        translatedName: translationName,
+      });
 
-      console.log(`Texto traducido: ${task.description} → ${translation}`);
+      console.log(`Nombre traducido: ${task.name} → ${translationName}`);
+      console.log(
+        `Descripción traducida: ${task.description} → ${translationDesc}`,
+      );
       return null;
     } catch (error) {
-      console.error("Error al traducir el texto:", error);
+      console.error("Error al traducir la tarea:", error);
       return null;
     }
   });
